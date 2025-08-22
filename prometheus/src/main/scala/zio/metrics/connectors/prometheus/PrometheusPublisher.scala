bb4ef53c30e088fd8610592f124cd777ae609692
Jules Ivanic <jules.ivanic@gmail.com>
2025-08-22 13:03:39 +1000

    Optimise Prometheus related code (#103)
    * Optimise Prometheus related code
    
    * Make `PrometheusPublisher` follow the service pattern

--- prometheus/src/main/scala/zio/metrics/connectors/prometheus/PrometheusPublisher.scala
+++ prometheus/src/main/scala/zio/metrics/connectors/prometheus/PrometheusPublisher.scala
@@ -2,20 +2,21 @@ package zio.metrics.connectors.prometheus
 
 import zio._
 
-class PrometheusPublisher private (
-  current: Ref[String]) {
+trait PrometheusPublisher {
+  def get(implicit trace: Trace): UIO[String]
+  def set(next: String)(implicit trace: Trace): UIO[Unit]
+}
 
-  def get(implicit trace: Trace): UIO[String] =
-    current.get
-
-  def set(next: String)(implicit trace: Trace): UIO[Unit] =
-    current.set(next)
+final class PrometheusPublisherLive private[prometheus] (current: Ref[String]) extends PrometheusPublisher {
+  def get(implicit trace: Trace): UIO[String]             = current.get
+  def set(next: String)(implicit trace: Trace): UIO[Unit] = current.set(next)
 }
 
 object PrometheusPublisher {
 
-  def make = for {
-    current <- Ref.make[String]("")
-  } yield new PrometheusPublisher(current)
+  def make: UIO[PrometheusPublisher] =
+    for {
+      current <- Ref.make[String]("")
+    } yield new PrometheusPublisherLive(current)
 
 }
